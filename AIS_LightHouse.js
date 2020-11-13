22748832e43c9e00e65814e16d74929059a09f55
Duban Restrepo <dubanp@uninorte.edu.co>
2020-11-13 17:04:30 -0500

    Last character from message removed

--- AIS_LightHouse.js
+++ AIS_LightHouse.js
@@ -7,7 +7,9 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var ais = require('ais-stream-decoder');
 const { default: AisDecoder } = require('ais-stream-decoder');
-
+decoder = new AisDecoder();
+decoder.on('error', err => console.error(err));
+decoder.on('data', decodedMessage => msg = decodedMessage);
 var app = express();
 var server = require('http').Server(app);
 var io = socketio.listen(server);
@@ -17,9 +19,7 @@ require('dotenv').config();
 //Render CSS
 app.use(express.static(__dirname));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-decoder = new AisDecoder();
-decoder.on('error', err => console.error(err));
-decoder.on('data', decodedMessage => msg = decodedMessage);
+
 
 
 
@@ -27,6 +27,7 @@ decoder.on('data', decodedMessage => msg = decodedMessage);
 socket.on('message', (content, rinfo) => {
     console.log(`Server got: ${content} from ${rinfo.address}:${rinfo.port}`);
     var nmea = content.toString();
+    nmea = nmea.substring(0, nmea.length);
     console.log(nmea)
     decoder.write(content);
     io.sockets.emit('udp message', msg);
