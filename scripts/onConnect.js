020994abf96efa53e3b0b332c6b8bbd6e6f983c5
Duban Restrepo <dubanp@uninorte.edu.co>
2020-11-16 16:15:05 -0500

    v0.0.1

--- scripts/onConnect.js
+++ scripts/onConnect.js
@@ -14,7 +14,7 @@ markers = [];
 try {
     $.get('/startup', function(message) {
         locations = message['b'];
-        data = [mmsi.length];
+        dataTemplate(mmsi.length)
         mmsi = message['a'];
         console.log(message);
         startShips();
@@ -40,5 +40,47 @@ function startShips() {
             infoWindow.close();
         });
         markers.push(marker);
+    }
+}
+
+function dataTemplate(fin) {
+    for (u = 0; u < fin; u++) {
+        var newData = {};
+        //mensaje tipo 1
+        newData['navStatus '] = null;
+        newData['rateOfTurn '] = null;
+        newData['speedOverGround '] = null;
+        newData['accuracy '] = null;
+        newData['lon '] = null;
+        newData['lat '] = null;
+        newData['courseOverGround '] = null;
+        newData['heading '] = null;
+        newData['utcSecond '] = null;
+        newData['specialManoeuvre '] = null;
+        newData['raim '] = null;
+        newData['radio '] = null;
+        newData['type '] = null;
+        newData['channel '] = null;
+        newData['repeat '] = null;
+        newData['mmsi '] = null;
+        //Mensaje tipo 5
+        newData['aisVersion '] = null;
+        newData['imo '] = null;
+        newData['callsign '] = null;
+        newData['name '] = null;
+        newData['typeAndCargo '] = null;
+        newData['dimBow '] = null;
+        newData['dimStern '] = null;
+        newData['dimPort '] = null;
+        newData['dimStarboard '] = null;
+        newData['epfd '] = null;
+        newData['etaMonth '] = null;
+        newData['etaDay '] = null;
+        newData['etaHour '] = null;
+        newData['etaMinute '] = null;
+        newData['draught '] = null;
+        newData['destination '] = null;
+        newData['dte '] = null;
+        data.push(newData);
     }
 }
