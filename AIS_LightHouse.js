d9119b3bf7411592b4767c35b88f34b51e92792d
Duban Restrepo <dubanp@uninorte.edu.co>
2020-11-27 17:50:48 -0500

    Port 80 server

--- AIS_LightHouse.js
+++ AIS_LightHouse.js
@@ -186,6 +186,6 @@ app.get('/startup', (request, response) => {
 
 
 socket.bind(50001);
-server.listen(10000, () => {
+server.listen(80, () => {
     console.log('Servidor abierto en puerto 10000');
 });
